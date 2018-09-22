c99b832c84a52d59579d662f3c5bd51c54a89f12
okapie <tatigreuze@yahoo.co.jp>
2018-09-22 10:45:38 +0900

    Add index.html as a template.
    Modify TaskController to display task list.

--- src/main/kotlin/com/example/todolist/TaskController.kt
+++ src/main/kotlin/com/example/todolist/TaskController.kt
@@ -1,19 +1,23 @@
 package com.example.todolist
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping("tasks")
 class TaskController {
 
     @GetMapping("")
-    fun index(): String {
+    fun index(model: Model): String {
         val tasks = listOf(
             Task(1, "hoge", false),
             Task(2, "fuga", true)
         )
-        return tasks.toString()
+        model.addAttribute("tasks", tasks)
+        // Specify html file.
+        return "tasks/index"
     }
 }
