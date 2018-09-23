30c186028fd55a982a00e449d4dd526a56188e5a
okapie <tatigreuze@yahoo.co.jp>
2018-09-23 15:07:47 +0900

    Define create method.

--- src/main/kotlin/com/example/todolist/TaskController.kt
+++ src/main/kotlin/com/example/todolist/TaskController.kt
@@ -2,7 +2,10 @@ package com.example.todolist
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -21,5 +24,15 @@ class TaskController(private val taskRepository: InMemoryTaskRepository) {
     @GetMapping("new")
     fun new(form: TaskCreateForm): String {
         return "tasks/new"
+    }
+
+    @PostMapping("")
+    fun create(@Validated form: TaskCreateForm,
+            bindingResult: BindingResult): String {
+        if (bindingResult.hasErrors())
+            return "tasks/new"
+        val content = requireNotNull(form.content)
+        taskRepository.create(content)
+        return "redirect:/tasks"
     }
 }
