311a7e65bb09aa01edef9778bdabc637747afbd9
okapie <tatigreuze@yahoo.co.jp>
2018-09-24 09:17:39 +0900

    Add handleNotFoundException method for error handling.

--- src/main/kotlin/com/example/todolist/TaskController.kt
+++ src/main/kotlin/com/example/todolist/TaskController.kt
@@ -1,5 +1,8 @@
 package com.example.todolist
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
@@ -54,4 +57,8 @@ class TaskController(private val taskRepository: InMemoryTaskRepository) {
         taskRepository.update(newTask)
         return "redirect:/tasks"
     }
+
+    @ExceptionHandler(NotFoundException::class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    fun handleNotFoundException(): String = "tasks/not_found"
 }
